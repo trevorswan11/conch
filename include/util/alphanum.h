2af2da058a868836ee24447c739b489e7bef48a6
Trevor Swan <tcs94@case.edu>
2025-11-04 11:54:32 -0500

    added some comments to alphanum, created files for hash_map; it is time

--- include/util/alphanum.h
+++ include/util/alphanum.h
@@ -6,12 +6,18 @@ static inline bool is_digit(char byte) {
     return '0' <= byte && byte <= '9';
 }
 
+// Checks if the given byte is a letter.
+//
+// An underscore is considered a letter.
 static inline bool is_letter(char byte) {
     bool lower = 'a' <= byte && byte <= 'z';
     bool upper = 'A' <= byte && byte <= 'Z';
     return lower || upper || byte == '_';
 }
 
+// Checks if the given byte is a whitespace character.
+//
+// Includes all common whitespace bytes.
 static inline bool is_whitespace(char byte) {
     return byte == ' ' || byte == '\t' || byte == '\n' || byte == '\r';
 }
