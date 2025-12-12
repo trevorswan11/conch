6a46d29555720bf2c5a35aa603f39123c121d46f
trevorswan11 <tcs94@case.edu>
2025-12-12 14:41:02 -0500

    Formatting

--- tests/semantic/test_semantic.cpp
+++ tests/semantic/test_semantic.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Const / var declarations") {
         const std::vector<const char*> inputs = {
             "const v := 5;",
         };
-        
+
         for (const auto& i : inputs) {
             test_analyze(i);
         }
