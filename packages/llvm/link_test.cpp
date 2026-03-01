44da82299a72ae97d3109c816541ec4d89921193
trevorswan11 <tcs94@case.edu>
2026-02-28 20:34:34 -0500

    working on macos too! updated link_test to print builtin and runtime default target, can look to merge very soon!

--- packages/llvm/link_test.cpp
+++ packages/llvm/link_test.cpp
@@ -1,10 +1,28 @@
 #include <print>
 
+#include <llvm/Config/llvm-config.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/MC/TargetRegistry.h>
+
 #include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/TargetSelect.h>
+
+#include <llvm/TargetParser/Host.h>
+#include <llvm/TargetParser/Triple.h>
 
 auto main(int argc, char** argv) -> int {
     llvm::InitLLVM    test{argc, argv};
     llvm::LLVMContext ctx;
-    std::println("Hello, World!");
+
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    std::string TargetTriple  = llvm::sys::getDefaultTargetTriple();
+    std::string BuiltInTriple = LLVM_DEFAULT_TARGET_TRIPLE;
+
+    std::println("Built-in: {}", BuiltInTriple);
+    std::println("Runtime:  {}", TargetTriple);
 }
